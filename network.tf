7c9d7b0644d66d99e60d80ba45005f116040e5ac
Tigran Arabadjyan <arabadj@orgrimmar.local>
2020-04-10 17:36:13 +0400

    add network.tf

---
+++ network.tf
@@ -0,0 +1,65 @@
+
+resource "azurerm_network_security_group" "firewall" {
+  name                = "Firewall"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  security_rule {
+    name                       = "ICMP_ANY"
+    priority                   = 101
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "ICMP"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+    description                = ""
+  }
+  security_rule {
+    name                       = "SSH_ANY"
+    priority                   = 201
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_ranges    = ["2233","2222","22"]
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "RDP_TRUST"
+    priority                   = 301
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefixes    = ["178.160.250.112/28", "37.252.68.159"]
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "Network"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  address_space       = ["10.81.0.0/16"]
+}
+
+resource "azurerm_subnet" "servers" {
+  name                      = "Servers"
+  resource_group_name       = azurerm_resource_group.main.name
+  virtual_network_name      = azurerm_virtual_network.main.name
+  address_prefix            = "10.81.10.0/24"
+}
+
+ resource "azurerm_subnet_network_security_group_association" "servers_firewall" {
+   subnet_id                 = azurerm_subnet.servers.id
+   network_security_group_id = azurerm_network_security_group.firewall.id
+}
+
+
+
+
+
