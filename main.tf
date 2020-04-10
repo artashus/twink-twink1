60a7bcee6f58ec9ea28f2859357b5631e334e77e
Tigran Arabadjyan <arabadj@orgrimmar.local>
2020-04-10 11:33:08 +0400

    move provider to main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,16 @@ locals {
   loc_rgrp_location = "westeurope"
 }
 
+terraform {
+  required_version = "=0.12.24"
+}
+
+provider "azurerm" {
+  version         = "~> 2.5"
+  features {}
+}
+
+
 resource "azurerm_resource_group" "rgrp" {
   name     = local.loc_rgrp_name
   location = local.loc_rgrp_location
